6bee1ea0762b193d137ba2f3dbe69a531a967afe
Tamara Kuiper <tsnegosk@charlotte.edu>
2025-06-19 17:28:01 -0400

    Fixed value error condition

--- Assignment2/cashier.py
+++ Assignment2/cashier.py
@@ -6,11 +6,21 @@ class Cashier:
         """Returns the total calculated from coins inserted."""
         print("Please insert coins.")
         total = 0
-        total += int(input("How many quarters?: ")) * 0.25
-        total += int(input("How many dimes?: ")) * 0.10
-        total += int(input("How many nickels?: ")) * 0.05
-        total += int(input("How many pennies?: ")) * 0.01
-        return round(total, 2)
+        total += self._get_coin_input("How many quarters?: ", 0.25)
+        total += self._get_coin_input("How many dimes?: ", 0.10)
+        total += self._get_coin_input("How many nickels?: ", 0.05)
+        total += self._get_coin_input("How many pennies?: ", 0.01)
+        return total
+
+    def _get_coin_input(self, prompt, value):
+        while True:
+            try:
+                entry = input(prompt)
+                if entry == '':
+                    return 0
+                return int(entry) * value
+            except ValueError:
+                print("Please insert a valid whole number or press enter for 0.")
 
 
 
